792c4eaf0a8bb876d545289f7a8b745c940b33a1
GudmundurBB <gudmundurbr281@nemi.tskoli.is>
2017-01-25 14:27:34 +0000

    3 línur auka og lokar

--- Forrit.py
+++ Forrit.py
@@ -2,4 +2,7 @@
 textfile = open('file.txt','w')
 textfile.write('Python er ez')
 textfile.close()
-
+textfile = open('file.txt','a')
+textfile.write(' Þetta er fyrsta forritið mitt í python ')
+textfile.write(' og það var létt')
+textfile.close()
